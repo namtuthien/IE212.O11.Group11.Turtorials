ccd585eba483bd990c1f7baf92e8d8884e7cbd6d
namtuthien <leducmanh123kt@gmail.com>
2023-12-18 02:12:40 +0700

    Add turtorial06 - tutorial11

---
+++ turtorial10/streaming/detector.py
@@ -0,0 +1,59 @@
+import json
+import os
+from joblib import load
+import logging
+from multiprocessing import Process
+
+import numpy as np
+
+from utils import create_producer, create_consumer
+from settings import TRANSACTIONS_TOPIC, TRANSACTIONS_CONSUMER_GROUP, ANOMALIES_TOPIC, NUM_PARTITIONS
+
+model_path = os.path.abspath('./model/isolation_forest.joblib')
+
+
+def detect():
+    consumer = create_consumer(topic=TRANSACTIONS_TOPIC, group_id=TRANSACTIONS_CONSUMER_GROUP)
+
+    producer = create_producer()
+
+    clf = load(model_path)
+
+    while True:
+        message = consumer.poll(timeout=50)
+        if message is None:
+            continue
+        if message.error():
+            logging.error("Consumer error: {}".format(message.error()))
+            continue
+
+        # Message that came from producer
+        record = json.loads(message.value().decode('utf-8'))
+        data = record["data"]
+        print(data)
+        prediction = clf.predict(data)
+        if prediction[0] == 1:
+            print('Normal')
+
+        # If an anomaly comes in, send it to anomalies topic
+        if prediction[0] == -1:
+            print('Abnormal')
+            score = clf.score_samples(data)
+            record["score"] = np.round(score, 3).tolist()
+
+            _id = str(record["id"])
+            record = json.dumps(record).encode("utf-8")
+
+            producer.produce(topic=ANOMALIES_TOPIC, value=record)
+            producer.flush()
+            print(record)
+            print('Alert sent!')
+    consumer.close()
+
+
+# The following block ensures that this code is executed only in the main module
+if __name__ == '__main__':
+    # One consumer per partition
+    for _ in range(NUM_PARTITIONS):
+        p = Process(target=detect)
+        p.start()
